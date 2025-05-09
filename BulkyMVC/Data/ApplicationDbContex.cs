d8e25679ba9899e759ccde1d3b82f5de094366ea
Rajorshi Das <das2107060@stud.kuet.ac.bd>
2025-05-09 20:58:02 +0600

    added Data folder and ApplicationDbContex

---
+++ BulkyMVC/Data/ApplicationDbContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyMVC.Data
+{
+    public class ApplicationDbContex: DbContext
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex>options) : base(options)
+        {
+
+        }
+      
+    }
+}
